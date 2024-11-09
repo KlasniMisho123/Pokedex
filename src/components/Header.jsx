83d47f4a97b5c0602aebd801bcd3eeea82d008eb
KlasniMisho123 <mikheili.silagava.1@btu.edu.ge>
2024-11-09 22:32:30 +0400

    Header Commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
 export default function Header() {
     return(
         <header>
-            <button></button>
+            <button className="open-nav-button">
+                <i className="fa-solid fa-bars"></i>
+            </button>
+            <h1 className="text-gradient">Pokedex</h1>
         </header>
     )
 }
