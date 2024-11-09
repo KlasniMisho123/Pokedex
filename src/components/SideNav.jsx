cd891f0402e822b01a9b74d79085d3ef7b02b035
KlasniMisho123 <mikheili.silagava.1@btu.edu.ge>
2024-11-09 21:58:06 +0400

    Navbar Search Commit

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,19 +1,35 @@
+import { useState } from "react"
 import { first151Pokemon, getFullPokedexNumber } from "../utils"
 
 export default function SideNav(props) {
 
   const {selectedPokemon, setSelectedPokemon } = props
 
-  function handleNavbarClick(target) {
-    console.log("target: ", target)
-  }
+  const [searchValue, setSearchValue] = useState("")
+  const filteredPokemon = first151Pokemon.filter((ele, eleIndex) => {
+    // if full pokedex number includes current search value 
+    // return true
+    if(toString(getFullPokedexNumber(eleIndex)).includes (searchValue)) {return true}
+
+    // if pokemon name includes current search value,
+    // return true
+
+    //otherise, exclude value from array
+    
+  })
+  
 
   return(
       <nav>
         <div className={"header"}> 
             <h1 className="text-gradient">Pokedex</h1>
         </div>
-          <input />
+          <input
+           value={searchValue} 
+           onChange={(e)=>{
+            setSearchValue(e.target.value)
+           }}
+           />
           {first151Pokemon.map((pokemon, pokemonIndex) => {
             return(
               <button key={pokemonIndex} className={'nav-card ' + (pokemonIndex === selectedPokemon ? 'nav-card-selected ': " ")} 
